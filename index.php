8f50d421eb11ebccad018d35ba8268e87cbe3969
shrutizgit <shrutisharma.ica@gmail.com>
2024-11-29 12:34:24 +0530

    new file

--- index.php
+++ index.php
@@ -23,7 +23,38 @@
 							<div class="form-group text-center justify-content-center">
 							
 								<select style="width: 220px; height: 45px;" name="city" id="city" class="form-control demo-default" required>
-	<option value="">-- Select --</option><option value="Kanpur" >Kanpur</option><option value="Lucknow" >Lucknow</option><option value="Ghaziabad" >Ghaziabad</option><option value="Agra" >Agra</option><option value="Moradabad" >Moradabad</option><option value="Meerut" >Meerut</option><option value="Varanasi" >Varanasi</option><option value="Prayagraj" >Prayagraj</option><option value="Bareilly" >Bareilly</option><option value="Aligarh" >Aligarh</option><option value="Saharanpur" >Saharanpur</option><option value="Gorakhpur" >Gorakhpur</option><option value="Gautam Buddha Nagar" >Gautam Buddha Nagar</option><option value="Firozabad" >Firozabad</option><option value="Jafarabad" >Jafarabad</option><option value="Jhal Magsi" >Jhal Magsi</option><option value="Kalat" >Kalat</option><option value="Kech" >Kech</option><option value="Kharan" >Kharan</option><option value="Khuzdar" >Khuzdar</option><option value="Kohlu" >Kohlu</option><option value="Lasbela" >Lasbela</option><option value="Loralai" >Loralai</option><option value="Mastung" >Mastung</option><option value="Musakhel" >Musakhel</option><option value="Naseerabad" >Naseerabad</option><option value="Nushki" >Nushki</option><option value="Panjgur" >Panjgur</option><option value="Pishin" >Pishin</option><option value="Qilla Abdullah" >Qilla Abdullah</option><option value="Qilla Saifullah" >Qilla Saifullah</option><option value="Quetta" >Quetta</option><option value="Sibi" >Sibi</option><option value="Zhob" >Zhob</option><option value="Ziarat" >Ziarat</option><optgroup title="Federally Administered Tribal Areas (FATA" label="&raquo; Federally Administered Tribal Areas (FATA"></optgroup><option value="Bajaur Agency" >Bajaur Agency</option><option value="Khyber Agency" >Khyber Agency</option><option value="Kurram Agency" >Kurram Agency</option><option value="Mohmand Agency" >Mohmand Agency</option><option value="North Waziristan Agency" >North Waziristan Agency</option><option value="Orakzai Agency" >Orakzai Agency</option><option value="South Waziristan Agency" >South Waziristan Agency</option><optgroup title="Islamabad Capital" label="&raquo; Islamabad Capital"></optgroup><option value="Islamabad" >Islamabad</option><optgroup title="North-West Frontier Province (NWFP)" label="&raquo; North-West Frontier Province (NWFP)"></optgroup><option value="Abbottabad" >Abbottabad</option><option value="Bannu" >Bannu</option><option value="Batagram" >Batagram</option><option value="Buner" >Buner</option><option value="Charsadda" >Charsadda</option><option value="Chitral" >Chitral</option><option value="Dera Ismail Khan" >Dera Ismail Khan</option><option value="Dir Lower" >Dir Lower</option><option value="Dir Upper" >Dir Upper</option><option value="Hangu" >Hangu</option><option value="Haripur" >Haripur</option><option value="Karak" >Karak</option><option value="Kohat" >Kohat</option><option value="Kohistan" >Kohistan</option><option value="Lakki Marwat" >Lakki Marwat</option><option value="Malakand" >Malakand</option><option value="Mansehra" >Mansehra</option><option value="Mardan" >Mardan</option><option value="Nowshera" >Nowshera</option><option value="Peshawar" >Peshawar</option><option value="Shangla" >Shangla</option><option value="Swabi" >Swabi</option><option value="Swat" >Swat</option><option value="Tank" >Tank</option><optgroup title="Punjab" label="&raquo; Punjab"></optgroup><option value="Alipur" >Alipur</option><option value="Attock" >Attock</option><option value="Bahawalnagar" >Bahawalnagar</option><option value="Bahawalpur" >Bahawalpur</option><option value="Bhakkar" >Bhakkar</option><option value="Chakwal" >Chakwal</option><option value="Chiniot" >Chiniot</option><option value="Dera Ghazi Khan" >Dera Ghazi Khan</option><option value="Faisalabad" >Faisalabad</option><option value="Gujranwala" >Gujranwala</option><option value="Gujrat" >Gujrat</option><option value="Hafizabad" >Hafizabad</option><option value="Jhang" >Jhang</option><option value="Jhelum" >Jhelum</option><option value="Kasur" >Kasur</option><option value="Khanewal" >Khanewal</option><option value="Khushab" >Khushab</option><option value="Lahore" >Lahore</option><option value="Layyah" >Layyah</option><option value="Lodhran" >Lodhran</option><option value="Mandi Bahauddin" >Mandi Bahauddin</option><option value="Mianwali" >Mianwali</option><option value="Multan" >Multan</option><option value="Muzaffargarh" >Muzaffargarh</option><option value="Nankana Sahib" >Nankana Sahib</option><option value="Narowal" >Narowal</option><option value="Okara" >Okara</option><option value="Pakpattan" >Pakpattan</option><option value="Rahim Yar Khan" >Rahim Yar Khan</option><option value="Rajanpur" >Rajanpur</option><option value="Rawalpindi" >Rawalpindi</option><option value="Sahiwal" >Sahiwal</option><option value="Sargodha" >Sargodha</option><option value="Sheikhupura" >Sheikhupura</option><option value="Shekhupura" >Shekhupura</option><option value="Sialkot" >Sialkot</option><option value="Toba Tek Singh" >Toba Tek Singh</option><option value="Vehari" >Vehari</option><optgroup title="Sindh" label="&raquo; Sindh"></optgroup><option value="Badin" >Badin</option><option value="Dadu" >Dadu</option><option value="Ghotki" >Ghotki</option><option value="Hyderabad" >Hyderabad</option><option value="Jacobabad" >Jacobabad</option><option value="Jamshoro" >Jamshoro</option><option value="Karachi" >Karachi</option><option value="Kashmore" >Kashmore</option><option value="Khairpur" >Khairpur</option><option value="Larkana" >Larkana</option><option value="Matiari" >Matiari</option><option value="Mirpur Khas" >Mirpur Khas</option><option value="Naushahro Feroze" >Naushahro Feroze</option><option value="Nawabshah" >Nawabshah</option><option value="Qambar Shahdadkot" >Qambar Shahdadkot</option><option value="Sanghar" >Sanghar</option><option value="Shikarpur" >Shikarpur</option><option value="Sukkur" >Sukkur</option><option value="Tando Allahyar" >Tando Allahyar</option><option value="Tando Muhammad Khan" >Tando Muhammad Khan</option><option value="Tharparkar" >Tharparkar</option><option value="Thatta" >Thatta</option><option value="Umerkot" >Umerkot</option></select>
+	<option value="">-- Select --</option>
+	<option value="Andhra Pradesh" >Andhra Pradesh</option>
+	<option value="Arunachal Pradesh" >Arunachal Pradesh</option>
+	<option value="Assam" >Assam</option>
+	<option value="Bihar" >Bihar</option>
+	<option value="Chhatisgarh" >Chhatisgarh</option>
+	<option value="Goa" >Goa</option>
+	<option value="Gujarat" >Gujarat</option>
+	<option value="Haryana" >Haryana</option>
+	<option value="Himachal Pradesh" >Himachal Pradesh</option>
+	<option value="Jharkhand" >Jharkhand</option>
+	<option value="Karnataka" >Karnataka</option>
+	<option value="Kerala" >Kerala</option>
+	<option value="Madhya Pradesh" >Madhya Pradesh</option>
+	<option value="Maharashtra" >Maharashtra</option>
+	<option value="Manipur" >Manipur</option>
+	<option value="Meghalaya" >Meghalaya</option>
+	<option value="Mizoram" >Mizoram</option>
+	<option value="Nagaland" >Nagaland</option>
+	<option value="Odisha" >Odisha</option>
+	<option value="Punjab" >Punjab</option>
+	<option value="Rajasthan" >Rajasthan</option
+	><option value="Sikkim" >Sikkim</option>
+	<option value="Tamil Nadu" >Tamil Nadu</option>
+	<option value="Telangana" >Telangana</option>
+	<option value="Tripura" >Tripura</option>
+	<option value="Uttar Pradesh" >Uttar Pradesh</option>
+	<option value="Uttarakhand" >Uttarakhand</option>
+	<option value="West Bengal" >West Bengal</option>
+	<option value="Delhi" >Delhi</option>
+	<option value="Laddakh" >Laddakh</option>
+</select>
 							</div>
 							<div class="form-group center-aligned">
 								<select name="blood_group" id="blood_group" style="padding: 0 20px; width: 220px; height: 45px;" class="form-control demo-default text-center margin10px">
@@ -55,9 +86,19 @@
 						<h1 class="text-center"  style="color: white; font-weight: 700;padding: 10px 0 0 0;">Donate The Blood</h1>
 						<hr class="white-bar">
 						<p class="text-center pera-text">
-							We are a group of exceptional programmers; our aim is to promote education. If you are a student, then contact us to secure your future. We deliver free international standard video lectures and content. We are also providing services in Web & Software Development.
+						Welcome to Donate the blood, a dedicated platform committed to saving lives through the power of blood donation. Our mission is to connect generous donors with hospitals, clinics, and healthcare providers in urgent need of blood supplies. We believe that every donation has the potential to change lives, providing patients with the vital support they need during medical treatments, surgeries, and emergencies.
 
-							We are a group of exceptional programmers; our aim is to promote education. If you are a student, then contact us to secure your future. We deliver free international standard video lectures and content. We are also providing services in Web & Software Development.
+At Donate the blood, we aim to raise awareness about the importance of blood donation and create a seamless, user-friendly experience for those willing to make a life-saving contribution. Whether you're a first-time donor or a regular blood donor, our platform makes it easy to find donation centers, schedule appointments, and track your contributions.
+
+Together, we can create a compassionate community where every drop of blood counts. Join us in our mission to ensure that blood is always available for those who need it the most.
+
+Your donation can be the difference between life and death.
+
+Feel free to adapt it to your website’s specific tone, style, and goals.
+
+
+
+
 						</p>
 						<a href="#" class="btn btn-default btn-lg text-center center-aligned">Become a Life Saver!</a>
 					</div>
